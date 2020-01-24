f061fe7fdfe660154de24cab1380ebf8bd8bddc1
yogendra saxena <yogendrasaxena56@gmail.com>
2020-01-24 19:51:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -37,71 +37,20 @@ for (let i = 0; i < 500; i++) {
 
 //===========
 
-// let x = 200;
-// let velocity = 10;
-// function animate() {
-//   requestAnimationFrame(animate);
-//   c.clearRect(0, 0, window.innerWidth, window.innerHeight);
-//   c.beginPath();
-//   c.arc(x, 200, 30, 0, Math.PI * 2, false);
-//   c.strokeStyle = "blue";
-//   c.stroke();
-//   if (x >= innerWidth || x <= 0) {
-//     velocity = -velocity;
-//   }
-//   x = x + velocity;
-// }
-
-// animate();
-//=============
-
-//Draw line with mouse drag
-
-var ctx = canvas.getContext("2d");
-
-var canvasx = $(canvas).offset().left;
-var canvasy = $(canvas).offset().top;
-var last_mousex = (last_mousey = 0);
-var mousex = (mousey = 0);
-var mousedown = false;
-
-//Mousedown
-$(canvas).on("mousedown", function(e) {
-  last_mousex = parseInt(e.clientX - canvasx);
-  last_mousey = parseInt(e.clientY - canvasy);
-  mousedown = true;
-});
-
-//Mouseup
-$(canvas).on("mouseup", function(e) {
-  mousedown = false;
-});
-
-//Mousemove
-$(canvas).on("mousemove", function(e) {
-  mousex = parseInt(e.clientX - canvasx);
-  mousey = parseInt(e.clientY - canvasy);
-  if (mousedown) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height); //clear canvas
-    ctx.beginPath();
-    ctx.moveTo(last_mousex, last_mousey);
-    ctx.lineTo(mousex, mousey);
-    ctx.strokeStyle = "black";
-    ctx.lineWidth = 10;
-    ctx.lineJoin = ctx.lineCap = "round";
-    ctx.stroke();
+let x = 200;
+let velocity = 10;
+function animate() {
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, window.innerWidth, window.innerHeight);
+  c.beginPath();
+  c.arc(x, 200, 30, 0, Math.PI * 2, false);
+  c.strokeStyle = "blue";
+  c.stroke();
+  if (x >= innerWidth || x <= 0) {
+    velocity = -velocity;
   }
-  //Output
-  $("#output").html(
-    "current: " +
-      mousex +
-      ", " +
-      mousey +
-      "<br/>last: " +
-      last_mousex +
-      ", " +
-      last_mousey +
-      "<br/>mousedown: " +
-      mousedown
-  );
-});
+  x = x + velocity;
+}
+
+animate();
+//=============
